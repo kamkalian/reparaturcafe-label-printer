753bfc568b3530721222065fc04f12c3b5e213bb
kamkalian <kurm_de+github@pm.me>
2026-03-16 15:59:54 +0100

    Implemented .env

--- print_qrcode.py
+++ print_qrcode.py
@@ -1,8 +1,19 @@
+import os
 import requests
 import shutil
 import brother_ql
 from brother_ql.raster import BrotherQLRaster
 from brother_ql.backends.helpers import send
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_URL = os.getenv("BASE_URL", "").rstrip("/")
+USERNAME = os.getenv("USERNAME", "")
+PASSWORD = os.getenv("PASSWORD", "")
+
+if not BASE_URL:
+    raise ValueError("BASE_URL ist nicht in der .env Datei gesetzt.")
 
 # Using USB connected printer 
 PRINTER_IDENTIFIER = '/dev/usb/lp1'
@@ -14,13 +25,13 @@ def sendToPrinter():
     send(print_data, PRINTER_IDENTIFIER)
 
     
-token_url = "https://reparaturcafe-dev.it-awo.de/fastapi/token"
+token_url = f"{BASE_URL}/token"
 token_header = {"Content-Type": "application/x-www-form-urlencoded"}
-token_data = {"username": "", "password": ""}
+token_data = {"username": USERNAME, "password": PASSWORD}
 
 token_res = requests.post(token_url, data = token_data, headers = token_header)
 
-qrcode_all_url = "https://reparaturcafe-dev.it-awo.de/fastapi/qrcode/all"
+qrcode_all_url = f"{BASE_URL}/qrcode/all"
 qrcode_all_headers = {
     "Content-Type": "application/json",
     "Authorization": "Bearer " + token_res.json()["access_token"]
@@ -28,7 +39,7 @@ qrcode_all_headers = {
 
 qrcodes_res = requests.get(qrcode_all_url, headers = qrcode_all_headers)
 for qrcode in qrcodes_res.json():    
-    label_url = "https://reparaturcafe-dev.it-awo.de/fastapi/qrcode/create_label?task_id=" + str(qrcode["task_id"])
+    label_url = f"{BASE_URL}/qrcode/create_label?task_id=" + str(qrcode["task_id"])
     label_headers = {
     "Content-Type": "application/json"
     }
@@ -37,7 +48,7 @@ for qrcode in qrcodes_res.json():
     with open('qrcode.jpeg', 'wb') as f:
         shutil.copyfileobj(label_res.raw, f)
     sendToPrinter()
-    url = "https://reparaturcafe-dev.it-awo.de/fastapi/qrcode/complete?qrcode_id=" + str(qrcode["id"])
+    url = f"{BASE_URL}/qrcode/complete?qrcode_id=" + str(qrcode["id"])
     headers = {
     "Content-Type": "application/json"
     }
